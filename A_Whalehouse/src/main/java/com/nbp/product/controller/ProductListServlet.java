f924284fe0ea32a07736f25cf107e64be9396bac
hy802 <hy802@SJYOU>
2024-05-26 15:50:15 +0900

    유승주 커밋2

---
+++ A_Whalehouse/src/main/java/com/nbp/product/controller/ProductListServlet.java
@@ -0,0 +1,52 @@
+package com.nbp.product.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nbp.product.model.DTO.Product;
+import com.nbp.product.model.service.ProductService;
+
+/**
+ * Servlet implementation class ProductListServlet
+ */
+@WebServlet("/product/productlist.do")
+public class ProductListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int lastNo = Integer.parseInt(request.getParameter("lastNo"));
+		int firstNo = Integer.parseInt(request.getParameter("firstNo"));
+		System.out.println(lastNo+" "+firstNo);
+		List<Product> result = new ProductService().selectProductAll(firstNo,lastNo);
+	
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
